ff42c49a14bdb6106dc2c3571a4d1eab9b1c050d
andy <andersgee@gmail.com>
2017-11-03 13:17:12 +0100

    pulled compose.jl from master

--- compose.jl
+++ compose.jl
@@ -1,20 +1,9 @@
 module COMPOSE
 
 import NPZ
+import Distributions
 include("grid.jl")
 include("checkpoint.jl")
-
-function sample(y)
-  p = y./sum(y)
-  table = cumsum(p)
-  r=rand()
-  for n=1:length(y)
-    if table[n]>r
-      return n
-    end
-  end
-  return length(y)
-end
 
 function randTriad()
   # Picks a Major or minor triad in either rootposition, first or second inversion.
@@ -24,106 +13,93 @@ function randTriad()
   return notes[rand(1:12) + pattern[rand(1:size(pattern,1)),:]]
 end
 
-function compose(L, d, bsz, gridsize)
+function compose(L,d,bsz,gridsize)
   # pre-allocate
   N, C, fn, bn = GRID.linearindexing(gridsize)
+  Wenc, benc, Wdec, bdec = GRID.encodevars(L,d,N,bsz)
   W, b, g, mi, mo, hi, ho, Hi, WHib = GRID.gridvars(N,C,d,bsz)
 
-  # load trained model
-  #fname="trained/trained_bsz64_seqlen500_20500steps_3p43loss.jld"
-  #fname="trained/trained_bsz64_seqlen500_23500steps_3p18loss.jld"
-  #fname="trained/trained_bsz8_seqlen500.jld"
-  #fname="trained/trained_bsz4_seqlen4000.jld"
-  fname="trained/trained_bsz32_seqlen1440.jld"
-  Wenc, benc, W, b, Wdec, bdec = CHECKPOINT.load_model(fname)
-
   # setup a sequence
-  seqdim=1
-  projdim=2
-  x = [zeros(L,bsz) for i=1:gridsize[seqdim]]
-  z = [zeros(L,bsz) for i=1:gridsize[seqdim]]
-  
-  # initialize a song
-  K = 1000 # Length of song
-  generated=zeros(256,K) #initialize song
-  generated[randTriad(),1]=1 # prime the song with a random chord
-  
-  T=0 # Threshold
-  for I=1:10
-    for s=1:K
-      x[1] .= (generated[:,s].>0).*1
+  seqdim = 1
+  projdim = 2
+  seqlen=1000
+  x, z, t, ∇z, batch = GRID.sequencevars(L,bsz,gridsize,seqdim,seqlen)
+
+  #filename1 = string("trained/trained_bsz64_seqlen500.jld")
+  #filename1 = string("trained/trained_bsz4_seqlen4000.jld")
+  filename1 = "trained_bsz32_seqlen1440.jld"
+  Wenc, benc, W, b, Wdec, bdec = CHECKPOINT.load_model(filename1)
+
+  println("Composing ",bsz," songs in parallell")
+
+  for i=1:bsz
+    batch[i][randTriad(),1]=0.5
+  end
+
+  T = 0.12
+  for iteration=1:100
+
+    for batchstep=1:seqlen
+      for i=1:bsz
+        x[1][:,i] .= batch[i][:,batchstep].>0
+      end
 
       GRID.continue_sequence!(gridsize, seqdim, projdim, mi, hi, mo, ho, fn)
-
-      # fprop
       GRID.encode!(x, seqdim, projdim, Wenc, benc, mi, hi, fn,d)
       GRID.grid!(C,N,fn,WHib,W,b,g,mi,mo,hi,ho,Hi)
       GRID.decode!(ho, mo, seqdim, projdim, Wdec, bdec, z, bn, C)
       output = GRID.σ(z[1])
+      output=(output.>T).*output
 
-      if s<K
-        fill!(generated[:,s+1], 0.0)
-        for i=1:I
-          note=sample(output)
-          generated[note,s+1] = (output[note]>T)*output[note]
-        end
-      else
-        fill!(generated[:,1], 0.0)
-        for i=1:I
-          note=sample(output)
-          generated[note,1] = (output[note]>T)*output[note]
-        end
+      K=min(iteration,10)
+      for i=1:bsz
+        batch[i][:,batchstep+1] .*= 0.0
+        #notes=Distributions.wsample(1:L, output[:,i], i) #sample "i" notes mean 2 for song 2.. and 16 for song 16 etc
+        #notes=Distributions.wsample(1:L, output[:,i], iteration)
+        notes=Distributions.wsample(1:L, output[:,i], K)
+        batch[i][notes,batchstep+1] .= output[notes,i]
       end
 
+      #println(batchstep)
+
     end
-    println("After iteration ",I,", song has ",sum(generated[1:128,:].>0)," notes")
 
+    #put last output as first again
+    for i=1:bsz
+      batch[i][:,1] .= batch[i][:,end]
+    end
 
-    noteOn=generated[1:128,:]
-    #mean_noteOnCertainty = mean(noteOn[noteOn.>0])
-    #println("average noteOn certainty: ",mean_noteOnCertainty)
-    noteOff=generated[129:256,:]
-    #mean_noteOffCertainty = mean(noteOff[noteOff.>0])
-    #println("average noteOff certainty: ",mean_noteOffCertainty)
-    #T=mean_noteOnCertainty*0.95
+    println("\nAfter iteration ",iteration,":")
 
-    median_noteOnCertainty = median(noteOn[noteOn.>0])
-    T=median_noteOnCertainty*0.5
-    
-    println("Threshold is now ", T)
-    println()
+    #some info
+    for i=1:bsz
+      events=sum(batch[i][1:128,:].>0)
+      #events=sum(batch[i].>0)
+      #events=sum(batch[i][1:128,end-500:end].>0)
+      if events!=0
+        println("song ",i," has ",events, " events")
+      end
+    end
   end
-  #println("average note certainty: ",mean(generated[generated.>0]))
-  noteOn=generated[1:128,:]
-  mean_noteOnCertainty = mean(noteOn[noteOn.>0])
-  println("average noteOn certainty: ",mean_noteOnCertainty)
 
-  noteOff=generated[129:256,:]
-  mean_noteOffCertainty = mean(noteOff[noteOff.>0])
-  println("average noteOff certainty: ",mean_noteOffCertainty)
-#
-#  besthalf_noteOn = noteOn.>mean_noteOnCertainty
-#  #print(size(besthalf_noteOn))
-#
-#  println("song has ",sum(generated[1:128,:].>0), " notes")
-#  println("deleting least certain 50% of noteOn events")
-#  generated = vcat(besthalf_noteOn, noteOff)
-#  println("song has ",sum(generated[1:128,:].>0), " notes")
-#
-  filename="data/generated_npy/generated9.npy"
-  NPZ.npzwrite(filename, generated)
-  println("saved ", filename)
+  println("\nsaving songs:")
+  for i=1:bsz
+    filename=string("data/generated_npy/generated",i,".npy")
+    NPZ.npzwrite(filename, batch[i])
+    println(filename)
+  end
 
-  
 end
 
 function main()
-  L = 256
-  d = 256
+  L = 256 #input/output units
+  d = 256 #hidden units
+  #batchsize=16
   batchsize=8
   gridsize = [1,6]
   compose(L, d, batchsize, gridsize)
 end
 
 end
+
 COMPOSE.main()
